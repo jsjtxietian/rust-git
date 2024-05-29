8e7e8b6669c3c6bbbb76687a4f0a80ceb16cbf57
jsjtxietian <jsjtxietian@outlook.com>
2024-05-29 22:14:58 +0800

    Create blob file and reorg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use anyhow::Context;
 use clap::{Parser, Subcommand};
-use flate2::read::ZlibDecoder;
-use std::{
-    ffi::CStr,
-    fs,
-    io::{BufRead, BufReader, Read},
-};
+use std::fs;
+use std::path::PathBuf;
+
+pub(crate) mod commands;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -16,17 +13,19 @@ struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    /// Doc comment
     Init,
     CatFile {
         #[clap(short = 'p')]
         pretty_print: bool,
+
         object_hash: String,
     },
-}
+    HashObject {
+        #[clap(short = 'w')]
+        write: bool,
 
-enum Kind {
-    Blob,
+        file: PathBuf,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
@@ -43,50 +42,8 @@ fn main() -> anyhow::Result<()> {
         Command::CatFile {
             pretty_print,
             object_hash,
-        } => {
-            anyhow::ensure!(pretty_print, "mode must be gived without -p");
-            let mut f = std::fs::File::open(format!(
-                ".git/objects/{}/{}",
-                &object_hash[..2],
-                &object_hash[2..]
-            ))
-            .context("open in ./git/objects")?;
-            let z = ZlibDecoder::new(f);
-            let mut z = BufReader::new(z);
-            let mut buf = Vec::new();
-            z.read_until(0, &mut buf)
-                .context("Read header from .git/objects")?;
-            let header =
-                CStr::from_bytes_with_nul(&buf).expect("Exactly one nul and it's at the end");
-            let header = header
-                .to_str()
-                .context(".git/objects file header is not valid utf8")?;
-            let Some((kind, size)) = header.split_once(' ') else {
-                anyhow::bail!(
-                    ".git/objects file header does not start with a known type '{header}'"
-                );
-            };
-            let kind = match kind {
-                "blob" => Kind::Blob,
-                _ => anyhow::bail!("We do not know how to print a {kind}"),
-            };
-            let size = size
-                .parse::<u64>()
-                .context(".git/objects file header has invalid size: {size}")?;
-
-            let mut z = z.take(size);
-            match kind {
-                Kind::Blob => {
-                    let mut stdout = std::io::stdout().lock();
-                    let n = std::io::copy(&mut z, &mut stdout)
-                        .context("Write .git/objects to stdout")?;
-                    anyhow::ensure!(
-                        n == size.try_into().unwrap(),
-                        ".git/objects file {n} was not the expected size {size}"
-                    );
-                }
-            }
-        }
+        } => commands::cat_file::invoke(pretty_print, &object_hash)?,
+        Command::HashObject { write, file } => commands::hash_object::invoke(write, &file)?,
     }
 
     Ok(())
